3625403a9352060cd79aee765090287d65957b33
Starinin Andrey <starininandrey@gmail.com>
2022-05-31 18:56:04 +0300

    Add models

---
+++ Quiz.WebAPI/Models/AnswerModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Quiz.WebAPI.Models;
+
+public class AnswerModel
+{
+    public string Answer { get; set; }
+    public bool Correct { get; set; }
+}
---
+++ Quiz.WebAPI/Models/QuestionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Quiz.WebAPI.Models;
+
+public class QuestionModel
+{
+    public string Question { get; set; }
+    public List<AnswerModel> Answers { get; set; }
+}
