db0443885d1b1048c41d600744da546bd134f72f
Starinin Andrey <starininandrey@gmail.com>
2022-05-31 19:23:46 +0300

    Add DB

---
+++ Quiz.WebAPI/DB/DataBase.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Quiz.WebAPI.Models;
+
+namespace Quiz.WebAPI.DB;
+
+public class DataBase
+{
+    private const string str = "Data Source=usersdata.db";
+
+    public IEnumerable<QuestionModel> GetAllQuestions()
+    {
+        using var db = new SqliteConnection(str);
+        var sql = "SELECT question, answer, correct FROM tab_questions_answers JOIN tab_questions ON tab_questions_answers.question_id = tab_questions.id JOIN tab_answers ON tab_questions_answers.answer_id = tab_answers.id";
+        return db.Query<QuestionModel>(sql);
+    }
+}
---
+++ SQL/create_tables.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE tab_answers
+(
+    id      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    answer  TEXT    NOT NULL,
+    correct INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE tab_questions
+(
+    id       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    question TEXT    NOT NULL
+);
+
+CREATE TABLE tab_questions_answers
+(
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    question_id INTEGER NOT NULL,
+    answer_id   INTEGER NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES tab_questions (id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    FOREIGN KEY (answer_id) REFERENCES tab_answers (id) 
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
