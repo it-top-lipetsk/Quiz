38af73d19f84055be2cf282e40cc549be8c013b9
Starinin Andrey <starininandrey@gmail.com>
2022-05-31 20:07:47 +0300

    Add QuestionsController.cs

---
+++ Quiz.WebAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.WebAPI.DB;
+
+namespace Quiz.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/v1/questions")]
+public class QuestionsController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetAllTasks()
+    {
+        return new JsonResult(DataBase.GetAllQuestions());
+    }
+}
